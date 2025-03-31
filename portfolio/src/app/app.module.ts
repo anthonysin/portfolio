7cff3ad9b65b43a048454a757db6a33ab06436a8
Anthony Singh <101334291@georgebrown.ca>
2025-03-31 22:24:44 +0000

    New portfolio

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
